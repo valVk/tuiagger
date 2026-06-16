d102400bca736460bf0d5a88adb724baa57031cd
valVK <1130626+valVk@users.noreply.github.com>
2026-06-17 01:08:13 +0300

    Update utils barrel: replace storage.js with collectionResolver + persistence

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './colors.js';
 export * from './urlBuilder.js';
 export * from './curlGenerator.js';
-export * from './storage.js';
+export * from './collectionResolver.js';
+export * from './persistence.js';
 export * from './parser.js';
